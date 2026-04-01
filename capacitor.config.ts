3eee8d111d026720a61dc7bcfd80ae417cb8a2bb
Jesús Supayave <jsupayavebarequi@gmail.com>
2026-04-01 16:01:07 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'First store practice',
+  webDir: 'www'
+};
+
+export default config;
